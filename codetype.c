c6f3b30e8f9391dd9170ebdcded4ed2126e145a4
chenpiyanghappy <chenpiyang20070426@gmail.com>
2026-03-09 16:58:49 +0800

    提交「codetype.c」文件

---
+++ codetype.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <windows.h>
+int CodeType() {
+    SetConsoleOutputCP(65001);
+    printf("12 \n");
+    printf("计算的结果是：%d\n",10+2);
+    printf("输出字符%c \n",'a');
+    printf("输出字符串%f \n",1.1);
+    printf("八进制：%o \n",037);
+    printf("十六进制%x \n",0x37);
+    printf("输出多个常量%d,%d,%d \n",1,2,3);
+
+    char ch='a';
+    printf("计算符号常量：%c \n",ch);
+    return 0;
+}
+int main() {    // 必须添加 main 函数
+    CodeType();
+    return 0;
+}
