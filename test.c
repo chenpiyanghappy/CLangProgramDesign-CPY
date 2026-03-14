c82803645e82a7ddc72f0cebcf8eb2d836817320
chenpiyanghappy <chenpiyang20070426@gmail.com>
2026-03-14 14:55:50 +0800

    修改测试用例

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int test() {
-    int a=1,b=2;
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    printf("%d,%d",a,b);
+    printf("TEST USE ONLY");
+    printf("copyright (c) 2026 chenpiyang All rights reserved.");
+    char a,b,c;
+    a=getchar();
+    b=getchar();
+    c=getchar();
+    putchar(a);
+    putchar(b);
+    putchar(c);
     return 0;
 }
 int main() {
