c3e567de5fee9121b4a137a112b3d7f407423241
chenpiyanghappy <chenpiyang20070426@gmail.com>
2026-05-25 16:13:15 +0800

    提交教师示例文件、和测试文件

---
+++ C Files/test1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void test1 () {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    printf("sum = %d\n", sum);
+}
+int main () {
+    test1();
+    return 0;
+}
